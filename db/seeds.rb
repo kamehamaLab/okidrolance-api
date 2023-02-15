5d1ed0359e55cc601db21f76f859d695cde4aef0
maitake4211 <dmwajtgp4211@gmail.com>
2023-02-15 17:03:27 +0900

    dbの動作確認OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Device.create(location: "OpenLab")
+Device.create(location: "３F情報卒研室")
+Device.create(location: "４F倉庫")
+
+
