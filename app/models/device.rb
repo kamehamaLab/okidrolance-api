c8b2103f6ca8a1da03d6e9cea0014818bc7ca6b4
maitake4211 <dmwajtgp4211@gmail.com>
2023-02-15 15:56:13 +0900

    update: gem:annotateを設定した

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: devices
+#
+#  id         :bigint           not null, primary key
+#  location   :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Device < ApplicationRecord
   has_many :temperatures, dependent: :destroy
   has_many :water_temperatures, dependent: :destroy
--- app/models/water_temperature.rb
+++ app/models/water_temperature.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: water_temperatures
+#
+#  id         :bigint           not null, primary key
+#  w_temp     :float(24)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  devices_id :bigint           not null
+#
+# Indexes
+#
+#  index_water_temperatures_on_devices_id  (devices_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (devices_id => devices.id)
+#
 class WaterTemperature < ApplicationRecord
   belongs_to :device
 end
